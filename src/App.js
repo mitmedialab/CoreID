d5eecd661ffae3674d5ba0273bf0c41b7ce4fc8f
Jason Walsh <j@wal.sh>
2017-05-20 17:15:29 -0400

    Show mock data

--- src/App.js
+++ src/App.js
@@ -4,11 +4,41 @@ import React, { Component } from 'react';
 import claims from '../data/claims.json'
 import documents from '../data/documents.json'
 
+console.log(claims, documents)
+
 import logo from './logo.svg';
 
 import './App.css';
 
 class App extends Component {
+  renderClaim (claim) {
+    return (
+        <h3><tt>{JSON.stringify(claim.payload)}</tt></h3>
+    )
+  }
+
+  renderClaims () {
+    return (
+        <div className='claims-container'>
+        { claims.payload.map(claim => this.renderClaim(claim)) }
+        </div>
+    )
+  }
+
+  renderDocument (document) {
+    return (
+        <h3>{document.title}</h3>
+    )
+  }
+
+  renderDocuments () {
+    return (
+        <div className='documents-container'>
+        { documents.payload.map((document) => this.renderDocument(document)) }
+        </div>
+    )
+  }
+
   render() {
     return (
         <div className="App">
@@ -16,8 +46,10 @@ class App extends Component {
         <h2>Dashboard</h2>
         <h2>Profile</h2>
         <h2>Login</h2>
-        <h2>Identity Attributes</h2>
+        <h2>Identity Attributes / Claims</h2>
+        { this.renderClaims() }
         <h2>Documents</h2>
+        { this.renderDocuments() }
         </div>
     )
   }
