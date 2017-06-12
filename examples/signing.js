0445ef9e9423276bd92516bd44c64974fcd21cc9
Dazza Greenwood <dazza@civics.com>
2017-06-12 19:42:55 -0400

    Create signing.js

--- examples/signing.js
+++ examples/signing.js
@@ -1,10 +1,12 @@
 var bitcoin = require('bitcoin');
+//these are some configurations that will need adjusting based on the configuration of your bitcoin.conf file
 var client = new bitcoin.Client({
   host: 'localhost',
   port: 8332,
-  user: 'fred',
-  pass: 'fred'
+  user: 'user',
+  pass: 'pass'
 });
+//change the address to the address stored on your bitcoin core instance
 var address = "mjH9KTBnNyzdLifkr6xch5FkXnGdbf27mq"
 var message = "this is one more message"
 var signature1 = ""
